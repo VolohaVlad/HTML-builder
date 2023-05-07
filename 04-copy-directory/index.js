507a092971f4fe19c619dc069aee7fc7284784c6
VolohaVlad <volohanovich@mail.ru>
2023-05-07 19:58:32 +0300

    feat: added solution for 4 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const promises = require('fs/promises');
+const path = require('path');
+const process = require('process');
+const { stdout } = process;
 
+const sourcePath = path.join(__dirname, 'files');
+const destinationPath = path.join(__dirname, 'files-copy');
+
+promises
+  .rm(destinationPath, { recursive: true, force: true })
+  .then(() => promises.mkdir(destinationPath, { recursive: true }))
+  .then(() => promises.readdir(sourcePath))
+  .then((files) => {
+    files.forEach((file) => {
+      promises.copyFile(`${sourcePath}/${file}`, `${destinationPath}/${file}`);
+      stdout.write(`${destinationPath}${file}\n`);
+    });
+  })
+  .catch(function (error) {
+    stdout.write(error);
+  });
