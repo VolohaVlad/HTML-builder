0074ffbc86154692451fd2f2459eb7b9cc779a35
VolohaVlad <volohanovich@mail.ru>
2023-05-08 19:47:10 +0300

    fix: update file filter

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -7,23 +7,19 @@ const { stdout } = process;
 const sourcePath = path.join(__dirname, 'styles');
 const destinationPath = path.join(__dirname, 'project-dist/bundle.css');
 
-promises
-  .rm(destinationPath, { recursive: true, force: true })
-  .then(() => promises.readdir(sourcePath))
-  .then((files) =>
-    files.filter((file) => path.extname(path.join(sourcePath, file)) === '.css')
-  )
-  .then((files) =>
-    Promise.all(
-      files.map((file) =>
-        promises.readFile(path.join(sourcePath, file), 'utf-8')
-      )
-    )
-  )
-  .then((files) => {
+const createBundle = async () => {
+  try {
+    await promises.rm(destinationPath, { recursive: true, force: true });
     const output = fs.createWriteStream(destinationPath, { flags: 'w' });
-    files.forEach((fileData) => output.write(`${fileData}\n`));
-  })
-  .catch(function (error) {
+
+    const filesFromFolder = await promises.readdir(sourcePath, { withFileTypes: true });
+    const cssFiles = filesFromFolder.filter((file) => file.isFile() && path.extname(path.join(sourcePath, file.name)) === '.css');
+    const cssFileNames = cssFiles.map(file_1 => file_1.name);
+    const buffer = await Promise.all(cssFileNames.map((file_2) => promises.readFile(path.join(sourcePath, file_2), 'utf-8')));
+    buffer.forEach((fileData) => output.write(`${fileData}\n`));
+  } catch (error) {
     stdout.write(error);
-  });
+  }
+}
+
+Promise.all([createBundle()]);
